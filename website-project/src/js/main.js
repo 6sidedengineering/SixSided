8cfdc5fff122e1ec9f0cf51e20b9742b1b4f52cc
Lillian <l.macbeth.2020@bristol.ac.uk>
2025-07-07 16:58:41 +0100

    menuEdits

--- website-project/src/js/main.js
+++ website-project/src/js/main.js
@@ -4,13 +4,11 @@ document.addEventListener("DOMContentLoaded", function() {
         .then(data => {
             document.getElementById('menu-placeholder').innerHTML = data;
 
-            // Hex menu toggle logic
             var hexNav = document.getElementById('hexNav');
             var menuBtn = document.getElementById('menuBtn');
             if (hexNav && menuBtn) {
                 menuBtn.onclick = function() {
                     hexNav.classList.toggle('active');
-                    menuBtn.classList.toggle('active');
                 };
             }
         });
